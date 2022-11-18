9d118038bda60726d815655da037f84e4a183faa
Victor Juan <victorjuanrazer@gmail.com>
2022-11-18 05:43:03 -0300

    add:callback/anonimousFunctions

---
+++ Ex01 - Funcao/arrowFunction2.js
@@ -0,0 +1,11 @@
+class Pessoa {
+    constructor() {
+      this.idade = 0;
+      setInterval(() => {
+        this.idade++;
+        console.log(this.idade);
+      }, 1000);
+    }
+  }
+  
+  new Pessoa();
